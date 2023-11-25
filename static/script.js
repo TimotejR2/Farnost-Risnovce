dbd3504a4e33c3e269ed170bd1cdae7bd87362aa
Timotej Ružička <102914473+TimotejR2@users.noreply.github.com>
2023-11-25 17:22:57 +0100

    Add mobile version

--- static/script.js
+++ static/script.js
@@ -1,13 +1,25 @@
+var mainElement;
+var headerElement
 window.onload = function(){
+  mainElement = document.getElementById('main');
+  headerElement = document.getElementById("header");
     if (window.screen.availWidth > 700){
-        document.getElementById("mySidenav").style.width = "250px";
+      openNav();
     }
 }
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
+  mainElement.style.marginLeft = '250px';
+  headerElement.style.visibility = "hidden";
+  document.getElementById("header").style.maxHeight = '0';
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+  mainElement.style.marginLeft = '0';
+  headerElement.style.visibility = "visible";
+  document.getElementById("header").style.maxHeight = '120px';
 }
+
+
