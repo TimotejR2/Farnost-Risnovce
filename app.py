ac97a71f66305eecb7cfc1af56a9b125f82fc492
Timotej Ružička <102914473+TimotejR2@users.noreply.github.com>
2023-11-27 19:20:11 +0100

    Add auto news adding and login page.

--- app.py
+++ app.py
@@ -1,8 +1,58 @@
 from flask import Flask, render_template, request
+import sqlite3
 app = Flask(__name__)
+wrong = 0
+results = []
+@app.route('/update', methods=['GET', 'POST'])
+def update():
+    global wrong, results
+    if wrong < 5:
+        if request.method == 'POST':
+            if login(request.form['username'], request.form['password']):
+                wrong = 0
+                con = sqlite3.connect("data.db")
+                db = con.cursor()
+                db.execute("SELECT * FROM novinky;")
+                results = db.fetchall()
+                print (results)
+                return ("Hotovo")
+            else:
+                wrong += 1
+                return 'Login failed'
+
+    if request.method == 'GET':
+            return """
+                <!DOCTYPE html>
+                <html>
+                <body>
+                    <h1>Prihláste sa</h1>
+                    <form method="post" action="/update">
+                        <label for="username">Meno:</label>
+                        <input type="text" id="username" name="username"><br><br>
+                        <label for="password">Heslo:</label>
+                        <input type="password" id="password" name="password"><br><br>
+                        <input type="submit" value="Odoslať">
+                    </form>
+                </body>
+                </html>
+                """
+
+def login(username, password):
+    if username == 'root' and password == 'root':
+        return True
+    return False
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    global results
+    return render_template('index.html', list = results)
+
+
+
+
+
+
+
 
 @app.route('/foto')
 def foto():
@@ -29,4 +79,3 @@ def prednasky():
 @app.route('/publikacie')
 def publikacie():
     return render_template('publikacie.html')
-
