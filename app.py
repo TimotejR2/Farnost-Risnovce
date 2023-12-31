2ac4bcf0f8442d1fe8229c6f862602ac9a564a5c
TimotejR2 <timotej.ruzicka@gmail.com>
2023-12-31 15:30:09 +0100

    Pridanie sistému na pridávanie homílií

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ new = [] #Dic of every new post added manualy
 results = read() # List of all news from db + added after
 oznamy_list = [] # 
 log = [] # Track suspicious activity
+homilie_data=[] # Všetky homílie vo forme list s viacrými lists
 def error_log(code):
     global log
     log.append((error(code))[0])
@@ -157,30 +158,33 @@ def logs():
     global log
     return (log)
 
+@app.route('/homilie')
+def homilie():
+    global homilie_data
+    return render_template('homilie.html', list=homilie_data)
+
+@app.route('/homilie/update', methods=["GET", "POST"])
+def homilie_update():
+    if not user_logged_in("root"):
+        return redirect("/login")
+
+    if request.method == 'GET':
+        return get_html('static/homilie_input.html')
+
+    elif request.method == 'POST':
+        global homilie_data
+        list = []
+        list.append(request.form['datum'])
+        list.append(request.form['citanie'])
+        list.append(request.form['nazov'])
+        list.append(request.form['text'])
+        homilie_data.append(list)
+        return "Všetko prebehlo úspešne"
+
+#TODO:
 @app.route('/historia')
 def historia():
     return render_template('historia.html')
-
-@app.route('/homilie')
-def homilie():
-    a = ['Prvá adventná nedeľa', '(Mk 13, 33-37)', 'Otvorte srdcia...', '''        Keď odchádza syn alebo dcéra z domu rodičov na dlhší čas, rodičia sa snažia dať im primerané napomenutie, poučenie alebo nejakú radu, podľa ktorej sa majú riadiť. „Dávaj si pozor, dcéra moja, hlavne na zlé kamarátky!“, lúči sa matka s dcérou, ktorá ide na štúdiá. „Buď čestný, statočný a hlavne chlap!“, podáva ruku otec synovi, ktorý odchádza do zahraničia za prácou.
-        Cirkev je nám starostlivou matkou, ktorá chce do našich sŕdc vložiť isté poučenie, napomenutie, ktoré by nám pomohlo, aby sme nezišli zo správnej životnej cesty a aby sme vytrvali v dobrom až do konca.
-        Cirkev nám dnes predkladá v evanjeliu slová Pána Ježiša: „Bedlite a modlite sa, aby vás Pán nenašiel spať, ak príde nečakane!“ Tieto slová sú pre nás veľkou múdrosťou. Bedliť neznamená spať, váľať sa a leňošiť, ale každý deň si plniť povinnosti, žiť v zhode a láske s Bohom a ľuďmi.
-        Mnohí kresťania chcú bedliť tak, že zostanú stáť a nepohnute čakajú, kedy príde koniec sveta. Aké by to bolo smiešne v rodine, keby si otec sadol, zobral noviny a čakal na koniec sveta. Čo by to bola za matka, keby si zobrala štrikovanie a čakala na svoju smrť. Čo by to bol za mladík, ktorý by šiel do krčmy a tam čakal na Kristov príchod. Zdá sa nám to smiešne, ale povedzme si bolestnú pravdu. Mnohokrát to v našom živote skutočne takto vyzerá.
-        Určite ste už stretli rodinu, kde členovia žijú vedľa seba ako cudzinci. Jedia spoločne ten istý chlieb pri tom istom stole, ale navzájom sú si cudzí. Duševne sú si na kilometre vzdialení. A stačilo by len trochu sa usmiať a otvoriť ústa a hneď by sa v dome rozľahlo teplo a srdečnosť. Prečo sa stane, že manželia po niekoľkých rokoch sú už unudení, nemajú si čo povedať, nevedia sa tešiť. Vari čakajú na svoju smrť či koniec sveta?
-        Alebo dvadsaťročný mládenec každý večer nepozná nič iné len bar. Nezaujíma ho láska, úsmev dievčaťa, dotyk jej ruky. Už dávno všetko prežil. Mal dievča a bez lásky, bez hanby. Najedol sa a teraz potrebuje len zapiť sklamanie, žiaľ, nudu, smútok. Sadne do baru a víno, pivo či alkohol je jeho začiatok i koniec.
-       Prečo takto žijeme? Prečo si takto ničíme vzťahy? Prečo sa oberáme o radosť a šťastie? Je pravda, že záhaľka a postávanie má svoje čaro. V bare nájdeme kľud, pokoj i priateľov. Ale iba dočasne a veľmi povrchne. Zničíme si srdce, zničíme v sebe to, čo je tam ešte dobré. Možno tu nie ste tí, ktorí takto robia, ale odkážme to všetkým, ktorí takto konajú.
-        Čo my ostatní? V tomto týždni bude spoveď. Je to pre nás prvá možnosť ako by sme sa Kristovi otvorili. Ten Ježiš, ktorý má prísť na Vianoce, nech príde do našich sŕdc, ktoré sa takto otvoria. Tak veľmi vám prajem, aby sa každé srdce, nech by bolo akokoľvek zavreté, v tejto chvíli otvorilo. Prichádza Kristus. Nech sa uskutoční advent v nás, v našom vnútri.''']
-    b = ['Druhá adventná nedeľa', '(Mk 1, 1-8)', 'Vyrovnať cestu', '''        Akonáhle sa otvorili hranice po revolúcii, mnohí ľudia sa rozhodli cestovať do zahraničia a zoznámiť sa s krásou a životom iných krajov a národov. Vycestovať to znamenalo urobiť kvalitnú prípravu, zodpovedne sa prichystať na niečo nové.
-        „Pripravte cestu Pánovi!“, znie v dnešnom evanjeliu. Kam nás to posiela predchodca Pána, Ján Krstiteľ? Kam to máme pripraviť cestu?
-        Máme si dnes predovšetkým urovnať cestu do svojho vnútra. Ku svojmu ja máme cestu dobre známu, tá je dobre vychodená. Sme zatarasení sebou, svojimi problémami, ťažkosťami, radosťami. Ten vonkajšok máme pripravený. Mali by sme sa však pustiť iným smerom. Pozrieť sa do svojho kresťanského vnútra, do srdca. Mali by sme sa pozrieť na seba bez príkras, bez spoločenskej masky, bez divadielka, ktoré ľuďom predkladáme.
-        Každá dolina..., to je zamyslenie nad sebou, ak zotrvávame v hriechu, treba sa namáhať, pracovať na sebe. Modlitbou a pevnou vierou vyrovnávam cestu. Hora a pahorok, to je pýcha, vynášanie sa nad druhých. Krivé cesty..., to sú všetky pokrytectvá, farizejstvo, to sú myšlienky, slová a činy, ktoré nenesú v sebe nijakú úprimnosť.
-        Ďalej máme vyrovnať cestu k blížnemu. Nie blížneho využiť vo svoj prospech. Ján nás vyzýva, aby sme sa obrátili opačne, na cestu služby blížnemu. Ďalej máme urovnať svoju cestu k Bohu. Akú poznáme cestu k Bohu doteraz? Mnohí sa uspokoja s tým, že patria krstom do cirkvi, iným stačí, že prijali všetky sviatosti. Mnohí sa obrátia k Bohu len vtedy, keď im tečie do topánok: „Bože, pomôž!“ Mnohým cesta do kostola je akýmsi tajomným obchodom: „Ja som prišiel do kostola a ty mi, Bože, za to daj...!“
-        Ján Krstiteľ nás vyzýva, aby sme takéto cesty zanechali, aby sme nehľadali Boha ako obchodného partnera alebo agenta poisťovne pri pohrome a ťažkostiach, ale aby sme sa k nemu vydali cestou pokory a lásky ako jeho milované deti. Ide o to, aby sme sa Kristovi naplno oddali, otvorili, zmenili svoj život. A to je prijať nový životný štýl, pustiť Krista do svojho srdca a to zmení všetko a to je kresťanstvo. To predpokladá, že moja duša bude pohotovo oddaná Bohu a srdce bude nastavené na zmenu života. A to je to najťažšie. To chce urobiť jasný krok. A preto mnohí ľudia ostanú len pred touto bránou a nikdy nenájdu radosť a požehnanie s kresťanstva.
-        Urobme teda jasný krok: Prestaňme so starým spôsobom života, pomáhajme, pretrhnime hriešne známosti, buďme verní v manželstvách, zoberme za seba zodpovednosť pri výchove detí, hľadajme pravdu a Kristovo evanjelium.
-        Keď Bohu oddáme našu slobodu, až vtedy zostaneme slobodní. Keď Bohu oddáme naše srdce, až vtedy začneme milovať.''']
-    list = [a,b]
-    return render_template('homilie.html', list=list)
 
 @app.route('/kontakt')
 def kontakt():
