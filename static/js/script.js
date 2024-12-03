5bfbaa56475c8340be3e344713bb6500d6e19662
TimotejR <timotej.ruzicka@gmail.com>
2024-12-03 18:33:42 +0100

    improve problem with moveing navbar

--- static/js/script.js
+++ static/js/script.js
@@ -2,13 +2,9 @@
 var mainElement
 var headerElement
 
-// Open sidenav if screen width is more than 700, get main and header elements
 window.onload = function() {
-  if (window.screen.availWidth > 700) {
-    mainElement = document.getElementById('main');
-    headerElement = document.getElementById("header");
-    openNav();
-  }
+  mainElement = document.getElementById('main');
+  headerElement = document.getElementById("header");
 }
 
 // Highlight active link based on current path
@@ -39,6 +35,7 @@ function removeActiveClass() {
 // Open sidenav
 function openNav() {
   document.getElementById("sidenav").style.width = "250px";
+  document.getElementById("sidenav").style.visibility = "visible";
   mainElement.style.marginLeft = '250px';
   headerElement.style.visibility = "hidden";
   document.getElementById("header").style.maxHeight = '0';
@@ -47,6 +44,7 @@ function openNav() {
 // Close sidenav
 function closeNav() {
   document.getElementById("sidenav").style.width = "0";
+  document.getElementById("sidenav").style.visibility = "hidden";
   mainElement.style.marginLeft = '0';
   headerElement.style.visibility = "visible";
   document.getElementById("header").style.maxHeight = '120px';
@@ -60,11 +58,15 @@ function displayMenu(submenu) {
 // Hide submenu based on timeout
 function hideMenu(submenu, menu) {
   setTimeout(function() {
-    if (!document.querySelector(`#${submenu}:hover`) && !document.querySelector(`#${menu}:hover`)) {
-      document.getElementById(submenu).style.display = 'none';
+    var submenuElement = document.getElementById(submenu);
+    var menuElement = document.getElementById(menu);
+    
+    if (!submenuElement.matches(':hover') && !menuElement.matches(':hover')) {
+      submenuElement.style.display = 'none';
     }
   }, 100);
 }
+
 
 // Toggle menu visibility based on cursor support
 let isMenuVisible = false;
